2ae358cb1b4bcbab48c8f3800abea7693b004289
Manoj Mahalingam <manojlds@gmail.com>
2013-03-30 02:45:57 +0530

    Scaffolding yinstall

---
+++ install.ps1
@@ -0,0 +1,10 @@
+param(
+    $applications = @("ydeliver-mvc4"),
+    $buildNumber = "1.0.0",
+    $artifactsDir = "$(pwd)\build"
+    )
+
+$script:scriptDir = split-path $MyInvocation.MyCommand.Path -parent
+
+#Import-Module $script:scriptDir\BuildScripts\YDeliver.psm1
+Invoke-YInstall $applications -buildVersion $buildNumber -config @{ "conventions" = @{ "artifactsDir" = $artifactsDir } }
